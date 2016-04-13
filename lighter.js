5456168fbe3f8a3fc1ff6efae794ec536ea078ab
Andrey Shmatko <shmatuan@gmail.com>
2016-04-13 12:06:49 +0300

    blink opt

--- lighter.js
+++ lighter.js
@@ -1,41 +1,38 @@
 function lighterStart(timer_show, mod, colors, times, blinks) {
 
-    function getNext(type) {
-        type = type.split(' ')[0];
+    function findIColor(type) {
         var iNext = 0;
         colors.forEach(function (e, i) {
-            if (e.split(' ')[0] == type) {
-                iNext = i + 1;
+            if (e == type) {
+                iNext = i;
             }
         });
+        return iNext;
+    }
+
+    function getNext(type) {
+        var iNext = findIColor(type) + 1;
         if (iNext > colors.length - 1) iNext = 0;
         return colors[iNext];
     }
 
     function getTime(type) {
-        type = type.split(' ')[0];
-        var iNext = 0;
-        type = type.split(' ')[0];
-        colors.forEach(function (e, i) {
-            if (e.split(' ')[0] == type) {
-                iNext = i;
-            }
-        });
-        return times[iNext];
+        return times[findIColor(type)];
     }
 
-    function whatHappening(type, blink) {
+
+    function whatHappening(type) {
         $('#lighter' + mod + ' div').removeClass(colors.join(' ')).addClass('grey');
         type.split(' ').forEach(function (e) {
-            if (blink) $('#' + e + mod).removeClass('grey').addClass(e);
+            $('#' + e + mod).removeClass('grey').addClass(e);
         });
     }
 
     function blink_me(type) {
         var dont_blink;
-        var blink = false;
+        var blink = true;
         dont_blink = setInterval(function () {
-            whatHappening(type, blink);
+            $('#' + type + mod).removeClass(type).addClass('grey');
             blink = !blink;
             clearInterval(dont_blink);
         }, 500)
@@ -56,7 +53,7 @@ function lighterStart(timer_show, mod, colors, times, blinks) {
                 setTimer(next, getTime(next));
             }
             if (s >= 1) {
-                whatHappening(type, true);
+                whatHappening(type);
                 timer_show.html(s);
                 s--;
             }
@@ -64,10 +61,11 @@ function lighterStart(timer_show, mod, colors, times, blinks) {
     }
 
     timer_show.html(times[0]);
-    whatHappening(colors[0], true);
+    whatHappening(colors[0]);
     setTimer(colors[0], times[0]);
 }
 
 $(document).ready(function(){
+    //lighterStart($('#my_timer'), '', ['red green'], [1], [{type: 'green', s: 3}]);
     lighterStart($('#my_timer'), '', ['red', 'yellow red', 'green'], [10, 2, 15], [{type: 'green', s: 3}]);
 });
